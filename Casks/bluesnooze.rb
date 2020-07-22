031b958d2274000f82702da90a1232a810d57a69
Oliver Peate <oliverp@gmail.com>
2020-07-22 18:04:42 +0100

    Initial commit

---
+++ Casks/bluesnooze.rb
@@ -0,0 +1,9 @@
+cask "bluesnooze" do
+  name "Bluesnooze"
+  version "v1.0"
+  homepage "https://github.com/odlp/bluesnooze"
+  app "Bluesnooze.app"
+
+  sha256 "39ee3429c63fcfb733507b68a6ac033b6bdd45168b4b5ec8e62ab61963c191cf"
+  url "https://github.com/odlp/bluesnooze/releases/download/#{version}/Bluesnooze.zip"
+end
---
+++ README.md
@@ -0,0 +1,1 @@
+# Homebrew tap for [Bluesnooze](https://github.com/odlp/bluesnooze)
